875770996847669b89ed57444bc4734d19811ab6
Discrett <84587410+Disrett@users.noreply.github.com>
2026-01-07 16:19:17 +0100

    ajout barre de navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p><p>amen je suis un dieu tout puissant</p>
+        <Navbar />
         <a
           className="App-link"
           href="https://reactjs.org"
